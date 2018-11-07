9b8f68d3d71b975ac7686be8f633668e08e2124c
Aaron Tan <atjwen@outlook.my>
2018-11-07 00:57:33 +0000

    Created constructor methods in Alarm.java, outlined the workings of the driver class

--- ClockTest.java
+++ ClockTest.java
@@ -20,6 +20,7 @@ public class ClockTest {
         wake up message
          */
 
+        //Uncomment
         //Checks if badData is empty. If empty, display success & save data, if not display badData message
         /*if(alarm.getBadData() == null){
             JOptionPane.showMessageDialog(null, "Success", "Yes", JOptionPane.INFORMATION_MESSAGE);
